ed9f0df946fa6ea643fac4a8516f5a742adddc2c
Marcos Lautaro Molina <marcoslautaromolina@gmail.com>
2025-09-08 23:40:10 -0300

    complete el ejercicio 9 de javascript

--- JS/ejercicio-9.js
+++ JS/ejercicio-9.js
@@ -8,4 +8,19 @@ input: Hola mundo
 Outpot: oauo
 */
 
-const
+const frase = prompt("Ingrese la frase  'Hola mundo' ");
+
+if (frase.charAt(1) === "o") {
+  document.writeln(frase.charAt(1));
+}
+if (frase.charAt(3) === "a") {
+  document.writeln(frase.charAt(3));
+}
+if (frase.charAt(6) === "u") {
+  document.writeln(frase.charAt(6));
+}
+if (frase.charAt(9) === "o") {
+  document.writeln(frase.charAt(9));
+} else{
+    document.writeln('Frase no encontrada')
+}
